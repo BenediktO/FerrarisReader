23fd6ab15975d15b61c4fc9784071d256ae42db3
Benedikt Otto <beneot@web.de>
2022-09-11 09:53:12 +0200

    Enable multiple sensors

--- FerrarisReaderArduino/FerrarisReaderArduino.ino
+++ FerrarisReaderArduino/FerrarisReaderArduino.ino
@@ -26,14 +26,14 @@
 SCPI_Parser controller;
 
 
+
+#define NUM_CHANNELS 2
+
+
 #define ACTIVITY_LED_PIN 10
-#define IR_SENSOR A0
-#define IR_LED 9
 
-
-
-#define UPPER_THRESHOLD 50
-#define LOWER_THRESHOLD 20
+#define UPPER_THRESHOLD 100
+#define LOWER_THRESHOLD 50
 #define MIN_DURATION 20
 
 
@@ -41,21 +41,34 @@ SCPI_Parser controller;
 #define KWH_PER_TURN (1.0 / TURNS_PER_KWH)
 
 
+int IR_SENSORS[NUM_CHANNELS] = {A0, A1};
+int IR_LEDS = 9;  // common pin
+
 
 unsigned long last_time;
 int counter = 0;
 
-uint16_t active_value;
-uint16_t dark_value;
+uint16_t active_value[NUM_CHANNELS];
+uint16_t dark_value[NUM_CHANNELS];
 
-int high_counter = 0;
-int low_counter = 0;
-bool state = true;
-unsigned long ticks;
+int high_counter[NUM_CHANNELS];
+int low_counter[NUM_CHANNELS];
+bool state[NUM_CHANNELS];
+unsigned long ticks[NUM_CHANNELS];
 
 
-int last_tick;
-int second_last_tick;
+int last_tick[NUM_CHANNELS];
+int second_last_tick[NUM_CHANNELS];
+
+
+int get_channel_number(SCPI_P parameters) {
+  if (parameters.Size() == 0)
+    return 0;
+  int temp = String(parameters[0]).toInt();
+  if ((temp >= 0) && (temp < NUM_CHANNELS))
+    return temp;
+  return 0;
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -74,97 +87,105 @@ void Identify(SCPI_C commands, SCPI_P parameters, Stream& interface) {
 // Resets the Controller Board
 void Reset(SCPI_C commands, SCPI_P parameters, Stream& interface) {
   digitalWrite(ACTIVITY_LED_PIN, HIGH);
-  ticks = 0;
-  high_counter = 0;
-  low_counter = 0;
-  state = true;
+  for (int i=0; i<NUM_CHANNELS; i++) {
+    ticks[i] = 0;
+    high_counter[i] = 0;
+    low_counter[i] = 0;
+    state[i] = true;
+  }
   digitalWrite(ACTIVITY_LED_PIN, LOW);
 }
 
 void MeasureTicks(SCPI_C commands, SCPI_P parameters, Stream& interface) {
   digitalWrite(ACTIVITY_LED_PIN, HIGH);
-  interface.println(ticks);
+  interface.println(ticks[get_channel_number(parameters)]);
   digitalWrite(ACTIVITY_LED_PIN, LOW);
 }
 
 void MeasureDark(SCPI_C commands, SCPI_P parameters, Stream& interface) {
   digitalWrite(ACTIVITY_LED_PIN, HIGH);
-  interface.println(dark_value);
+  interface.println(dark_value[get_channel_number(parameters)]);
   digitalWrite(ACTIVITY_LED_PIN, LOW);
 }
 
 void MeasureActive(SCPI_C commands, SCPI_P parameters, Stream& interface) {
   digitalWrite(ACTIVITY_LED_PIN, HIGH);
-  interface.println(active_value);
+  interface.println(active_value[get_channel_number(parameters)]);
   digitalWrite(ACTIVITY_LED_PIN, LOW);
 }
 
 void MeasureEffective(SCPI_C commands, SCPI_P parameters, Stream& interface) {
   digitalWrite(ACTIVITY_LED_PIN, HIGH);
-  interface.println(active_value - dark_value);
+  int channel_number = get_channel_number(parameters);
+  interface.println(active_value[channel_number] - dark_value[channel_number]);
   digitalWrite(ACTIVITY_LED_PIN, LOW);
 }
 
 void MeasureEnergy(SCPI_C commands, SCPI_P parameters, Stream& interface) {
   digitalWrite(ACTIVITY_LED_PIN, HIGH);
-  interface.println(((float) ticks) / TURNS_PER_KWH, 4);
+  interface.println(((float) ticks[get_channel_number(parameters)]) / TURNS_PER_KWH);
   digitalWrite(ACTIVITY_LED_PIN, LOW);
 }
 
 void MeasurePower(SCPI_C commands, SCPI_P parameters, Stream& interface) {
   digitalWrite(ACTIVITY_LED_PIN, HIGH);
-  if (ticks < 2)
+  int channel_number = get_channel_number(parameters);
+  if (ticks[channel_number] < 2)
     interface.println(F("Invalid"));
-  else
-    interface.println(KWH_PER_TURN * 3600 / (last_tick - second_last_tick), 4);
+  else {
+    interface.println(KWH_PER_TURN * 3600 / (last_tick[channel_number] - second_last_tick[channel_number]), 4);
+  }
   digitalWrite(ACTIVITY_LED_PIN, LOW);
 }
 
 void MeasurePeriod(SCPI_C commands, SCPI_P parameters, Stream& interface) {
   digitalWrite(ACTIVITY_LED_PIN, HIGH);
-  if (ticks < 2)
+  int channel_number = get_channel_number(parameters);
+  if (ticks[channel_number] < 2)
     interface.println(F("Invalid"));
-  else
-    interface.println(last_tick - second_last_tick);
+  else {
+    interface.println(last_tick[channel_number] - second_last_tick[channel_number]);
+  }
   digitalWrite(ACTIVITY_LED_PIN, LOW);
 }
 
 void MeasureLastTick(SCPI_C commands, SCPI_P parameters, Stream& interface) {
   digitalWrite(ACTIVITY_LED_PIN, HIGH);
-  if (ticks < 1)
+  int channel_number = get_channel_number(parameters);
+  if (ticks[channel_number] < 1)
     interface.println(F("Invalid"));
   else
-    interface.println((millis() / 1000) - last_tick);
+    interface.println((millis() / 1000) - last_tick[channel_number]);
   digitalWrite(ACTIVITY_LED_PIN, LOW);
 }
 
-void tick() {
-  ticks++;
-  second_last_tick = last_tick;
-  last_tick = millis() / 1000;
+void tick(int index) {
+  ticks[index]++;
+  second_last_tick[index] = last_tick[index];
+  last_tick[index] = millis() / 1000;
 }
 
 
-void calculate_everything(uint16_t active_value, uint16_t dark_value) {
+void calculate_everything(uint16_t active_value, uint16_t dark_value, int index) {
   int difference = active_value - dark_value;
 
   if (difference > UPPER_THRESHOLD) {
-    high_counter++;
-    if (high_counter > MIN_DURATION)
-      state = true;
+    high_counter[index]++;
+    if (high_counter[index] > MIN_DURATION)
+      state[index] = true;
   }
   else {
-    high_counter = 0;
+    high_counter[index] = 0;
     if (difference < LOWER_THRESHOLD) {
-      low_counter++;
-      if (low_counter > MIN_DURATION) {
-        if (state)
-          tick();
-        state = false;
+      low_counter[index]++;
+      if (low_counter[index] > MIN_DURATION) {
+        if (state[index])
+          tick(index);
+        state[index] = false;
       }
     }
     else {
-      low_counter = 0;
+      low_counter[index] = 0;
     }
   }
 }
@@ -174,7 +195,7 @@ void calculate_everything(uint16_t active_value, uint16_t dark_value) {
 void setup() {
   // SCPI standard commands
   controller.RegisterCommand(F("*IDN?"), &Identify);
-  controller.RegisterCommand(F("*RST?"), &Reset);
+  controller.RegisterCommand(F("*RST"), &Reset);
 
   // MEASure commands
   controller.RegisterCommand(F("MEASure:TICKs?"), &MeasureTicks);
@@ -187,12 +208,21 @@ void setup() {
   controller.RegisterCommand(F("CALCulate:ENERgy?"), &MeasureEnergy);
   controller.RegisterCommand(F("CALCulate:POWer?"), &MeasurePower);
 
+  for (int i=0; i < NUM_CHANNELS; i++) {
+    ticks[i] = 0;
+    high_counter[i] = 0;
+    low_counter[i] = 0;
+    state[i] = true;
+  }
+
   Serial.begin(9600);
   // Wait until serial connection is established
   while (!Serial) {;}
 
   pinMode(ACTIVITY_LED_PIN, OUTPUT);
-  pinMode(IR_LED, OUTPUT);
+  pinMode(IR_LEDS, OUTPUT);
+  for (int i=0; i < NUM_CHANNELS; i++)
+    pinMode(IR_SENSORS[i], INPUT);
 
 
   #ifdef DEBUG
@@ -207,30 +237,28 @@ void loop()
 {
   controller.ProcessInput(Serial, "\n");
 
-
-  // if (Serial.available()) {
-  //   Serial.println(Serial.read());
-  // }
-
-
   if ((millis() - last_time) > 5) {
     last_time = millis();
     switch (counter) {
     case 0: {
-      digitalWrite(IR_LED, HIGH);   // First step: switch on LED
+      digitalWrite(IR_LEDS, HIGH);   // First step: switch on LEDs
       break;
       }
     case 1: {
-      active_value = analogRead(IR_SENSOR);   // Second step: measure active current
+      for (int i=0; i < NUM_CHANNELS; i++) {
+        active_value[i] = analogRead(IR_SENSORS[i]);   // Second step: measure active current
+      }
       break;
       }
     case 2: {
-      digitalWrite(IR_LED, LOW);   // Third step: switch off LED
+      digitalWrite(IR_LEDS, LOW);   // Thirds step: switch off LEDs
       break;
       }
     case 3: {
-      dark_value = analogRead(IR_SENSOR);   // Fourth step: measure dark current and evaluate
-      calculate_everything(active_value, dark_value);
+      for (int i=0; i < NUM_CHANNELS; i++) {
+        dark_value[i] = analogRead(IR_SENSORS[i]);   // Fourth step: measure dark current and evaluate
+        calculate_everything(active_value[i], dark_value[i], i);
+      }
       break;
       }
     }

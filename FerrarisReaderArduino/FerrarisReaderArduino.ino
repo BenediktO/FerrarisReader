86c0c578fe028036db3b244a89a1fa50c58558b1
Benedikt Otto <beneot@web.de>
2022-09-11 11:50:12 +0200

    Add some parameters, add query to check number of channels

--- FerrarisReaderArduino/FerrarisReaderArduino.ino
+++ FerrarisReaderArduino/FerrarisReaderArduino.ino
@@ -19,9 +19,11 @@
 
 #include "Arduino.h"
 
+#include <ctype.h>
+
 #include "Vrekrer_scpi_parser.h"
 
-#define DEBUG
+//#define DEBUG
 
 SCPI_Parser controller;
 
@@ -32,9 +34,9 @@ SCPI_Parser controller;
 
 #define ACTIVITY_LED_PIN 10
 
-#define UPPER_THRESHOLD 100
-#define LOWER_THRESHOLD 50
-#define MIN_DURATION 20
+#define DEFAULT_UPPER_THRESHOLD 100
+#define DEFAULT_LOWER_THRESHOLD 50
+#define DEFAULT_DURATION 20
 
 
 #define TURNS_PER_KWH 75
@@ -59,6 +61,9 @@ struct SensorData
   uint32_t ticks;
   int last_tick;
   int second_last_tick;
+  uint16_t upper_threshold;
+  uint16_t lower_threshold;
+  uint16_t duration;
 };
 
 
@@ -82,13 +87,16 @@ int get_channel_number(SCPI_P parameters) {
 
 
 void reset(void) {
-  struct SensorData sensor;
+  struct SensorData * sensor;
   for (int i=0; i<NUM_CHANNELS; i++) {
-    sensor = sensors[i];
-    sensor.ticks = 0;
-    sensor.high_counter = 0;
-    sensor.low_counter = 0;
-    sensor.state = true;
+    sensor = &sensors[i];
+    sensor->ticks = 0;
+    sensor->high_counter = 0;
+    sensor->low_counter = 0;
+    sensor->state = true;
+    sensor->upper_threshold = DEFAULT_UPPER_THRESHOLD;
+    sensor->lower_threshold = DEFAULT_LOWER_THRESHOLD;
+    sensor->duration = DEFAULT_DURATION;
   }
 }
 
@@ -183,6 +191,62 @@ void MeasureLastTick(SCPI_C commands, SCPI_P parameters, Stream& interface) {
   digitalWrite(ACTIVITY_LED_PIN, LOW);
 }
 
+void CountChannels(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+  digitalWrite(ACTIVITY_LED_PIN, HIGH);
+  interface.println(NUM_CHANNELS);
+  digitalWrite(ACTIVITY_LED_PIN, LOW);
+}
+
+// What the Fuck? Why is that not a standard library function???
+char *toLower(char *str, size_t len)
+{
+    char *str_l = calloc(len + 1, sizeof(char));
+
+    for (size_t i = 0; i < len; ++i) {
+        str_l[i] = tolower((unsigned char)str[i]);
+    }
+    return str_l;
+}
+
+void ConfSetThreshold(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+  digitalWrite(ACTIVITY_LED_PIN, HIGH);
+  struct SensorData * sensor = &sensors[get_channel_number(parameters)];
+  if (parameters.Size() >= 2) {
+    if (strcmp(toLower(commands[3], 3), "upp") == 0)
+      sensor->upper_threshold = String(parameters[1]).toInt();
+    else if (strcmp(toLower(commands[3], 3), "low") == 0)
+      sensor->lower_threshold = String(parameters[1]).toInt();
+  }
+  digitalWrite(ACTIVITY_LED_PIN, LOW);
+}
+
+void ConfGetThreshold(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+  digitalWrite(ACTIVITY_LED_PIN, HIGH);
+  struct SensorData sensor = sensors[get_channel_number(parameters)];
+  if (strcmp(toLower(commands[3], 3), "upp") == 0)
+    interface.println(sensor.upper_threshold);
+  else if (strcmp(toLower(commands[3], 3), "low") == 0)
+    interface.println(sensor.lower_threshold);
+  digitalWrite(ACTIVITY_LED_PIN, LOW);
+}
+
+void ConfSetDuration(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+  digitalWrite(ACTIVITY_LED_PIN, HIGH);
+  struct SensorData * sensor = &sensors[get_channel_number(parameters)];
+  if (parameters.Size() >= 2) {
+    sensor->duration = String(parameters[1]).toInt();
+  }
+  digitalWrite(ACTIVITY_LED_PIN, LOW);
+}
+
+void ConfGetDuration(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+  digitalWrite(ACTIVITY_LED_PIN, HIGH);
+  struct SensorData sensor = sensors[get_channel_number(parameters)];
+  interface.println(sensor.duration);
+  digitalWrite(ACTIVITY_LED_PIN, LOW);
+}
+
+
 void tick(struct SensorData * sensor_data) {
   sensor_data->ticks++;
   sensor_data->second_last_tick = sensor_data->last_tick;
@@ -193,16 +257,16 @@ void tick(struct SensorData * sensor_data) {
 void calculate_everything(struct SensorData * sensor_data) {
   int difference = sensor_data->active_value - sensor_data->dark_value;
 
-  if (difference > UPPER_THRESHOLD) {
+  if (difference > sensor_data->upper_threshold) {
     sensor_data->high_counter++;
-    if (sensor_data->high_counter > MIN_DURATION)
+    if (sensor_data->high_counter > sensor_data->duration)
       sensor_data->state = true;
   }
   else {
     sensor_data->high_counter = 0;
-    if (difference < LOWER_THRESHOLD) {
+    if (difference < sensor_data->lower_threshold) {
       sensor_data->low_counter++;
-      if (sensor_data->low_counter > MIN_DURATION) {
+      if (sensor_data->low_counter > sensor_data->duration) {
         if (sensor_data->state)
           tick(sensor_data);
         sensor_data->state = false;
@@ -227,10 +291,20 @@ void setup() {
   controller.RegisterCommand(F("MEASure:SENSor:DARK?"), &MeasureDark);
   controller.RegisterCommand(F("MEASure:SENSor:ACTIve?"), &MeasureActive);
   controller.RegisterCommand(F("MEASure:SENSor:EFFEctive?"), &MeasureEffective);
-  controller.RegisterCommand(F("MEASure:TICK:PERiod?"), &MeasurePeriod);
-  controller.RegisterCommand(F("MEASure:TICK:LAST?"), &MeasureLastTick);
+  controller.RegisterCommand(F("MEASure:TICKs:PERiod?"), &MeasurePeriod);
+  controller.RegisterCommand(F("MEASure:TICKs:LAST?"), &MeasureLastTick);
+
   controller.RegisterCommand(F("CALCulate:ENERgy?"), &MeasureEnergy);
   controller.RegisterCommand(F("CALCulate:POWer?"), &MeasurePower);
+
+  controller.RegisterCommand(F("CONFigure:SENSor:THREshold:UPPer"), &ConfSetThreshold);
+  controller.RegisterCommand(F("CONFigure:SENSor:THREshold:LOWer"), &ConfSetThreshold);
+  controller.RegisterCommand(F("CONFigure:SENSor:THREshold:UPPer?"), &ConfGetThreshold);
+  controller.RegisterCommand(F("CONFigure:SENSor:THREshold:LOWer?"), &ConfGetThreshold);
+  controller.RegisterCommand(F("CONFigure:SENSor:DURation"), &ConfSetDuration);
+  controller.RegisterCommand(F("CONFigure:SENSor:DURation?"), &ConfGetDuration);
+
+  controller.RegisterCommand(F("SYSTem:CHANnels?"), &CountChannels);
 
   reset();
 
